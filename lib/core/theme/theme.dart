cf3c668cdc602ddbeae950c076ce866b866084f8
aadarsha <basnetaadars123@gmail.com>
2026-02-21 22:40:30 +0545

    Created domain layer Auth Repository and added Failure class for error handling. Updated theme and signup page to reflect changes. Updated pubspec.yaml to include fpdart package.

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -4,6 +4,7 @@ import 'package:myapp/core/theme/app_pallete.dart';
 class AppTheme {
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
+    appBarTheme: const AppBarTheme(backgroundColor: AppPallete.backgroundColor),
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(27),
       enabledBorder: _border(),
